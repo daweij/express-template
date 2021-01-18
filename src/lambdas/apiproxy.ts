57b100aa1d86911bcea04c74c74449f250d77b5c
Daniel Weijmers <daniel@weijmers.com>
2021-01-18 22:41:08 +0100

    updated stuff ...

--- src/lambdas/apiproxy.ts
+++ src/lambdas/apiproxy.ts
@@ -2,10 +2,10 @@
 import awsServerlessExpress from "aws-serverless-express";
 import { Context } from "aws-lambda";
 import * as dotenv from "dotenv";
-import { router} from "../app";
+import { router } from "../app";
 
 dotenv.config();
-const server = awsServerlessExpress.createServer(router);
 
+const server = awsServerlessExpress.createServer(router);
 export const handler = (event: any, context: Context) =>
   awsServerlessExpress.proxy(server, event, context);
